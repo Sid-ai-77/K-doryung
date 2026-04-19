75c0f3013a4ec5fe6291d8fdf85b68ce2b854649
Sid-ai-77 <ptodak@naver.com>
2026-04-19 14:03:17 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'kdoryung-v1';
+const STATIC_ASSETS = [
+  '/k-doryung/',
+  '/k-doryung/index.html',
+  '/k-doryung/icon-192.png',
+  '/k-doryung/icon-512.png',
+  '/k-doryung/marker-doryung.png',
+  '/k-doryung/marker-baekgu.png',
+  '/k-doryung/manifest.json',
+  '/k-doryung/cheap-data.json',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(STATIC_ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        if (res.ok && new URL(e.request.url).origin === self.location.origin) {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+        }
+        return res;
+      });
+    })
+  );
+});
